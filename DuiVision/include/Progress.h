ac28e645c4db6859264349a390acd8bddece57b3
blueantst <blueantst@gmail.com>
2014-04-28 01:10:14 +0800

    进度条控件支持两张图片格式的进度条，支持圆角的进度条（使用九宫格方式画进度），可以指定进度条头部的长度。

--- DuiVision/include/Progress.h
+++ DuiVision/include/Progress.h
@@ -15,22 +15,38 @@ public:
 	int GetProgress() { return m_nProgress; };
 	BOOL SetRun(BOOL bRun, int nIndex = -1);
 
-	HRESULT OnAttributeRun(const CStringA& strValue, BOOL bLoading);
+	BOOL SetBackGroundImage(UINT nResourceID = 0, CString strType= TEXT("PNG"));
+	BOOL SetBackGroundImage(CString strImage = TEXT(""));
+	BOOL SetForeGroundImage(UINT nResourceID = 0, CString strType= TEXT("PNG"));
+	BOOL SetForeGroundImage(CString strImage = TEXT(""));
 	
 protected:
 	virtual	BOOL OnControlTimer();
 	virtual void DrawControl(CDC &dc, CRect rcUpdate);
+
+	HRESULT OnAttributeRun(const CStringA& strValue, BOOL bLoading);
+	HRESULT OnAttributeImageBackGround(const CStringA& strValue, BOOL bLoading);
+	HRESULT OnAttributeImageForeGround(const CStringA& strValue, BOOL bLoading);
 	
 public:
 	//过程索引
-	int m_nIndex;		// 当前计数(=m_nProgress)
-	int m_nMaxIndex;// 最大计数(100)
-	int m_nCount;		// 定时多少次计数增加一次
+	int				m_nIndex;			// 当前计数(=m_nProgress)
+	int				m_nMaxIndex;		// 最大计数(100)
+	int				m_nCount;			// 定时多少次计数增加一次
 	
-	int m_nProgress;	// 当前进度(0-100)
+	int				m_nProgress;		// 当前进度(0-100)
+
+	Image*			m_pImageBackGround;	// 背景图片
+	CSize			m_sizeBackGround;	// 背景图片大小
+	Image*			m_pImageForeGround;	// 前景图片
+	CSize			m_sizeForeGround;	// 前景图片大小
+	int				m_nHeadLength;		// 进度条图片头部长度
 
 	DUI_DECLARE_ATTRIBUTES_BEGIN()
 		DUI_INT_ATTRIBUTE("value", m_nProgress, FALSE)
 		DUI_CUSTOM_ATTRIBUTE("run", OnAttributeRun)
+		DUI_CUSTOM_ATTRIBUTE("img-back", OnAttributeImageBackGround)
+		DUI_CUSTOM_ATTRIBUTE("img-fore", OnAttributeImageForeGround)
+		DUI_INT_ATTRIBUTE("head-len", m_nHeadLength, FALSE)
 	DUI_DECLARE_ATTRIBUTES_END()
 };
--- DuiVision/source/Progress.cpp
+++ DuiVision/source/Progress.cpp
@@ -11,6 +11,12 @@ CDuiProgress::CDuiProgress(HWND hWnd, CDuiObject* pDuiObject)
 
 	SetBitmapCount(2);
 
+	m_pImageBackGround = NULL;
+	m_sizeBackGround = CSize(0, 0);
+	m_pImageForeGround = NULL;
+	m_sizeForeGround = CSize(0, 0);
+	m_nHeadLength = 0;
+
 	m_nProgress = 0;
 	SetProgress(0);
 }
@@ -25,6 +31,12 @@ CDuiProgress::CDuiProgress(HWND hWnd, CDuiObject* pDuiObject, UINT uControlID, C
 	m_nCount = 0;
 
 	SetBitmapCount(2);
+
+	m_pImageBackGround = NULL;
+	m_sizeBackGround = CSize(0, 0);
+	m_pImageForeGround = NULL;
+	m_sizeForeGround = CSize(0, 0);
+	m_nHeadLength = 0;
 
 	m_nProgress = 0;
 	SetProgress(nProgress);
@@ -69,6 +81,162 @@ HRESULT CDuiProgress::OnAttributeRun(const CStringA& strValue, BOOL bLoading)
 	return bLoading?S_FALSE:S_OK;
 }
 
+// 设置背景图片资源
+BOOL CDuiProgress::SetBackGroundImage(UINT nResourceID/* = 0*/, CString strType/*= TEXT("PNG")*/)
+{
+	if(m_pImageBackGround != NULL)
+	{
+		delete m_pImageBackGround;
+		m_pImageBackGround = NULL;
+	}
+
+	if(ImageFromIDResource(nResourceID, strType, m_pImageBackGround))
+	{
+		m_sizeBackGround.SetSize(m_pImageBackGround->GetWidth(), m_pImageBackGround->GetHeight());
+		return true;
+	}
+	return false;
+}
+
+// 设置背景图片文件
+BOOL CDuiProgress::SetBackGroundImage(CString strImage/* = TEXT("")*/)
+{
+	if(m_pImageBackGround != NULL)
+	{
+		delete m_pImageBackGround;
+		m_pImageBackGround = NULL;
+	}
+
+	m_pImageBackGround = Image::FromFile(strImage, TRUE);
+
+	if(m_pImageBackGround->GetLastStatus() == Ok)
+	{
+		m_sizeBackGround.SetSize(m_pImageBackGround->GetWidth(), m_pImageBackGround->GetHeight());
+		return true;
+	}
+	return false;
+}
+
+// 从XML设置背景图片信息属性
+HRESULT CDuiProgress::OnAttributeImageBackGround(const CStringA& strValue, BOOL bLoading)
+{
+	if (strValue.IsEmpty()) return E_FAIL;
+
+	// 通过Skin读取
+	CStringA strSkin = "";
+	if(strValue.Find("skin:") == 0)
+	{
+		strSkin = DuiSystem::Instance()->GetSkin(strValue);
+		if (strSkin.IsEmpty()) return E_FAIL;
+	}else
+	{
+		strSkin = strValue;
+	}
+
+	if(strSkin.Find(".") != -1)	// 加载图片文件
+	{
+		CString strImgFile = DuiSystem::GetSkinPath() + CA2T(strSkin, CP_UTF8);
+		if(strSkin.Find(":") != -1)
+		{
+			strImgFile = CA2T(strSkin, CP_UTF8);
+		}
+		if(!SetBackGroundImage(strImgFile))
+		{
+			return E_FAIL;
+		}
+	}else	// 加载图片资源
+	{
+		UINT nResourceID = atoi(strSkin);
+		if(!SetBackGroundImage(nResourceID, TEXT("PNG")))
+		{
+			if(!SetBackGroundImage(nResourceID, TEXT("BMP")))
+			{
+				return E_FAIL;
+			}
+		}
+	}
+
+	return bLoading?S_FALSE:S_OK;
+}
+
+// 设置前景图片资源
+BOOL CDuiProgress::SetForeGroundImage(UINT nResourceID/* = 0*/, CString strType/*= TEXT("PNG")*/)
+{
+	if(m_pImageForeGround != NULL)
+	{
+		delete m_pImageForeGround;
+		m_pImageForeGround = NULL;
+	}
+
+	if(ImageFromIDResource(nResourceID, strType, m_pImageForeGround))
+	{
+		m_sizeForeGround.SetSize(m_pImageForeGround->GetWidth(), m_pImageForeGround->GetHeight());
+		return true;
+	}
+	return false;
+}
+
+// 设置前景图片文件
+BOOL CDuiProgress::SetForeGroundImage(CString strImage/* = TEXT("")*/)
+{
+	if(m_pImageForeGround != NULL)
+	{
+		delete m_pImageForeGround;
+		m_pImageForeGround = NULL;
+	}
+
+	m_pImageForeGround = Image::FromFile(strImage, TRUE);
+
+	if(m_pImageForeGround->GetLastStatus() == Ok)
+	{
+		m_sizeForeGround.SetSize(m_pImageForeGround->GetWidth(), m_pImageForeGround->GetHeight());
+		return true;
+	}
+	return false;
+}
+
+// 从XML设置前景图片信息属性
+HRESULT CDuiProgress::OnAttributeImageForeGround(const CStringA& strValue, BOOL bLoading)
+{
+	if (strValue.IsEmpty()) return E_FAIL;
+
+	// 通过Skin读取
+	CStringA strSkin = "";
+	if(strValue.Find("skin:") == 0)
+	{
+		strSkin = DuiSystem::Instance()->GetSkin(strValue);
+		if (strSkin.IsEmpty()) return E_FAIL;
+	}else
+	{
+		strSkin = strValue;
+	}
+
+	if(strSkin.Find(".") != -1)	// 加载图片文件
+	{
+		CString strImgFile = DuiSystem::GetSkinPath() + CA2T(strSkin, CP_UTF8);
+		if(strSkin.Find(":") != -1)
+		{
+			strImgFile = CA2T(strSkin, CP_UTF8);
+		}
+		if(!SetForeGroundImage(strImgFile))
+		{
+			return E_FAIL;
+		}
+	}else	// 加载图片资源
+	{
+		UINT nResourceID = atoi(strSkin);
+		if(!SetForeGroundImage(nResourceID, TEXT("PNG")))
+		{
+			if(!SetForeGroundImage(nResourceID, TEXT("BMP")))
+			{
+				return E_FAIL;
+			}
+		}
+	}
+
+	return bLoading?S_FALSE:S_OK;
+}
+
 BOOL CDuiProgress::OnControlTimer()
 {
 	if(!m_bRunTime || !m_bIsVisible)
@@ -102,15 +270,34 @@ void CDuiProgress::DrawControl(CDC &dc, CRect rcUpdate)
 
 		m_memDC.BitBlt(0, 0, nWidth, nHeight, &dc, m_rc.left ,m_rc.top, SRCCOPY);
 
-		Graphics graphics(m_memDC);		
+		Graphics graphics(m_memDC);
 
-		DrawImageFrame(graphics, m_pImage, CRect(0, 0, nWidth, nHeight), 
-			0, 0, m_sizeImage.cx, m_sizeImage.cy, 2);
-
-		if(m_nProgress != 0)
+		if(m_pImageForeGround != NULL)	// 使用背景和前景图片画进度条
 		{
-			DrawImageFrame(graphics, m_pImage, CRect(0, 0, nWidth * m_nProgress / 100, nHeight), 
-				m_sizeImage.cx, 0, m_sizeImage.cx, m_sizeImage.cy, 2);
+			if(m_pImageBackGround != NULL)	// 画背景
+			{
+				DrawImageFrameMID(graphics, m_pImageBackGround, CRect(0, 0, nWidth, nHeight),
+					0, 0, m_sizeBackGround.cx, m_sizeBackGround.cy,
+					m_nHeadLength, 0, m_nHeadLength, 0);
+			}
+
+			if(m_nProgress != 0)	// 画前景
+			{
+				DrawImageFrameMID(graphics, m_pImageForeGround, CRect(0, 0, nWidth * m_nProgress / 100, nHeight),
+					0, 0, m_sizeForeGround.cx, m_sizeForeGround.cy,
+					m_nHeadLength, 0, m_nHeadLength, 0);
+			}
+		}else
+		if(m_pImage != NULL)	// 使用单张图片画进度条
+		{
+			DrawImageFrame(graphics, m_pImage, CRect(0, 0, nWidth, nHeight), 
+				0, 0, m_sizeImage.cx, m_sizeImage.cy, 2);
+
+			if(m_nProgress != 0)
+			{
+				DrawImageFrame(graphics, m_pImage, CRect(0, 0, nWidth * m_nProgress / 100, nHeight), 
+					m_sizeImage.cx, 0, m_sizeImage.cx, m_sizeImage.cy, 2);
+			}
 		}
 	}
 
