a05de0ffbe1801d834c2905cebb2f3793ae86239
blueantst <blueantst@gmail.com>
2014-09-08 13:49:31 +0800

    DuiHandler增加消息处理宏的定义，通过这些宏可以更清晰的定义消息处理映射，不用自己写OnDuiMessage处理函数。

--- DuiVision/include/DuiHandler.h
+++ DuiVision/include/DuiHandler.h
@@ -1,6 +1,70 @@
 // DuiVision message handler base class
 #pragma once
 
+//////////////////////////////////////////////////////////////////////////
+// DUI Message Handler declare
+#define DUI_DECLARE_MESSAGE_BEGIN(classname)                            \
+public:                                                             \
+    virtual LRESULT OnDuiMessage(                                   \
+        UINT uID, CString strName, UINT Msg, WPARAM wParam, LPARAM lParam)    \
+    {                                                               \
+        LRESULT nRet = __super::OnDuiMessage(                       \
+            uID, strName, Msg, wParam, lParam       \
+            );                                                      \
+        if (nRet)                                        \
+            return nRet;                                            \
+		DuiSystem::LogEvent(LOG_LEVEL_DEBUG,						\
+		_T("%s::OnDuiMessage:uID=%d, name=%s, msg=%d, wParam=%d, lParam=%d"),	\
+		_T(#classname), uID, strName, Msg, wParam, lParam);						\
+
+// 控件ID对应的事件处理
+#define DUI_CONTROL_ID_MESSAGE(controlid, msghandler)     \
+        if (controlid == uID)                            \
+        {                                                           \
+			nRet = msghandler(uID, strName, Msg, wParam, lParam);	\
+            if(nRet)                      \
+				return nRet;                      \
+        }                                                           \
+        else                                                        \
+
+// 控件ID和Msg对应的事件处理
+#define DUI_CONTROL_IDMSG_MESSAGE(controlid, msg, msghandler)     \
+        if (controlid == uID && msg == Msg)                            \
+        {                                                           \
+			nRet = msghandler(uID, strName, Msg, wParam, lParam);	\
+            if(nRet)                      \
+				return nRet;                      \
+        }                                                           \
+        else                                                        \
+
+// 控件名对应的事件处理
+#define DUI_CONTROL_NAME_MESSAGE(controlname, msghandler)     \
+        if (controlname == strName)                            \
+        {                                                           \
+			nRet = msghandler(uID, strName, Msg, wParam, lParam);	\
+            if(nRet)                      \
+				return nRet;                      \
+        }                                                           \
+        else                                                        \
+
+// 控件名和Msg对应的事件处理
+#define DUI_CONTROL_NAMEMSG_MESSAGE(controlname, msg, msghandler)     \
+        if (controlname == strName && msg == Msg)                            \
+        {                                                           \
+			nRet = msghandler(uID, strName, Msg, wParam, lParam);	\
+            if(nRet)                      \
+				return nRet;                      \
+        }                                                           \
+        else                                                        \
+
+#define DUI_DECLARE_MESSAGE_END()                              \
+            return 0;                                          \
+                                                                    \
+        return nRet;                                                \
+    }                                                               \
+
+
+
 class CDuiObject;
 
 // DUI事件处理类
